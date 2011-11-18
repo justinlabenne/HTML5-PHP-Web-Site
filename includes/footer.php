cc8420798f62793a8fea0e7fec897e06632bd29f
Justin LaBenne <justinlabenne@gmail.com>
2011-11-17 23:12:29 -0500

    Updated references to JQuery 1.7.0

--- includes/footer.php
+++ includes/footer.php
@@ -10,5 +10,5 @@
         </div>       
     </footer>
     
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="../js/jquery-1.6.2.min.js"><\/script>')</script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="../js/jquery-1.7.0.min.js"><\/script>')</script>
